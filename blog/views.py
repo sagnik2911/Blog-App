6f654ed268f601bc0d1da358aef1934e2096c158
sagnik2911 <sagnik2911@gmail.com>
2019-10-07 21:28:47 -0400

    Database and Migration

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 #from django.http import HttpResponse
+from .models import Post
 
 posts = [
     {
@@ -18,7 +19,7 @@ posts = [
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
     # return HttpResponse('<h1>Blog Home</h1>')
